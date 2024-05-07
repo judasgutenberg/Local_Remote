d466322b83c3f5acfe398343d4648afe326f7a78
Gus Mueller <gus@asecular.com>
2024-05-07 15:59:30 -0400

    reboot timeout config

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ extern const int controlDevice;
 extern const int sensorType; //SENSORS! -- we support these: 75 for LM75, 85 for BMP085, 180 for BMP180, 2301 for DHT 2301, 680 for BME680.  0 for no sensor.  No support for multiple sensors for now.
 extern const int locationId; //3 really is watchdog
 extern const int pollingGranularity; //how often to poll backend in seconds, 4 makes sense
-extern const int dataLoggingGranularity; //how often to store data in backend, 300 makes sense
+extern const int rebootPollingTimeout;
 extern const int connectionFailureRetrySeconds;
 extern const int connectionRetryNumber;
 extern const  int hiatusLengthOfUiUpdatesAfterUserInteraction;
