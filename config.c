e6c3cea47af930210f16c226ffbadb74a14030e2
Gus Mueller <gus@asecular.com>
2024-05-07 15:58:46 -0400

    reboot timeout config

--- config.c
+++ config.c
@@ -10,7 +10,7 @@ const int controlDevice  = 11;
 const int sensorType = 0; //2301;//2301;//680; //SENSORS! -- we support these: 75 for LM75, 85 for BMP085, 180 for BMP180, 2301 for DHT 2301, 680 for BME680.  0 for no sensor.  No support for multiple sensors for now.
 const int locationId = 11; //3 really is watchdog
 const int pollingGranularity = 4; //how often to poll backend in seconds, 4 makes sense
-const int dataLoggingGranularity = 300; //how often to store data in backend, 300 makes sense
+const int rebootPollingTimeout = 300; //if no polling in 300 seconds, reboot the ESP
 const int connectionFailureRetrySeconds = 4;
 const int connectionRetryNumber = 22;
 
