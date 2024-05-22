3f3f172170431f350cea9d6e213241382bb0433d
Gus Mueller <gus@asecular.com>
2024-05-22 10:52:19 -0400

    no need for sensor info

--- config.h
+++ config.h
@@ -8,7 +8,6 @@ extern const char* storagePassword; //to ensure someone doesn't store bogus data
 extern const char* dataSourceHost;
 extern const int weatherDevice;
 extern const int controlDevice; 
-extern const int sensorType; //SENSORS! -- we support these: 75 for LM75, 85 for BMP085, 180 for BMP180, 2301 for DHT 2301, 680 for BME680.  0 for no sensor.  No support for multiple sensors for now.
 extern const int locationId; //3 really is watchdog
 extern const int pollingGranularity; //how often to poll backend in seconds, 4 makes sense
 extern const int rebootPollingTimeout;
