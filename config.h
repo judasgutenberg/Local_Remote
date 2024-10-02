4ca7e17a6cc921ac4201a37f1d9afa5cea483995
Gus Mueller <gus@asecular.com>
2024-10-02 13:33:25 -0400

    naming convention change

--- config.h
+++ config.h
@@ -2,26 +2,24 @@
   
 extern const char* ssid; //mine was Moxee Hotspot83_2.4G
 extern const char* password;
-extern const char* storagePassword; //to ensure someone doesn't store bogus data on your server. should match value in config.php
+extern const char* storage_password; //to ensure someone doesn't store bogus data on your server. should match value in config.php
 //data posted to remote server so we can keep a historical record
 //url will be in the form: http://your-server.com:80/weather/data.php?data=
-extern const char* dataSourceHost;
-extern const int weatherDevice;
-extern const int controlDevice; 
-extern const int locationId; //3 really is watchdog
-extern const int pollingGranularity; //how often to poll backend in seconds, 4 makes sense
-extern const int rebootPollingTimeout;
-extern const int connectionFailureRetrySeconds;
-extern const int connectionRetryNumber;
-extern const int allowanceForBoot;
-extern const int hiatusLengthOfUiUpdatesAfterUserInteraction;
+extern const char* data_source_host;
+extern const int weather_device;
+extern const int control_device; 
+ 
+extern const int polling_granularity; //how often to poll backend in seconds, 4 makes sense
+extern const int reboot_polling_timeout;
+extern const int connection_retry_number;
+extern const int allowance_for_boot;
+extern const int hiatus_length_of_ui_updates_after_user_interaction;
 
-extern const int weatherRecordingInterval;
-extern const int weatherUpdateInterval;
-extern const int energyUpateInterval;
+extern const int weather_update_interval;
+extern const int energy_update_interval;
+ 
+extern const double temperature_delta_for_change;
+extern const double pressure_delta_for_change;
+extern const double humidity_delta_for_change;
 
-extern const double temperatureDeltaForChange;
-extern const double pressureDeltaForChange;
-extern const double humidityDeltaForChange;
-
-extern const int backlightTimeout;
+extern const int backlight_timeout;
