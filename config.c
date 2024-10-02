6dd8928cb09352bf5d53b1a4b90ee9575720fdee
Gus Mueller <gus@asecular.com>
2024-10-02 13:33:02 -0400

    naming convention change

--- config.c
+++ config.c
@@ -1,27 +1,24 @@
 
-const char* ssid = "xxxx"; //mine was Moxee Hotspot83_2.4G
-const char* password = "xxx";
-const char* storagePassword = "xxxx"; //to ensure someone doesn't store bogus data on your server. should match value in the storage_password column in you user table for your user
+const char* ssid = "your-ssid"; //mine was Moxee Hotspot83_2.4G
+const char* password = "your-wifi-password";
+const char* storage_password = "your-storage-password"; //to ensure someone doesn't store bogus data on your server. should match value in the storage_password column in you user table for your user
 //data posted to remote server so we can keep a historical record
 //url will be in the form: http://your-server.com:80/weather/data.php?data=
-const char* dataSourceHost = "xxxx.com";
-const int weatherDevice  = 13;
-const int controlDevice  = 11; 
- const int locationId = 11; //3 really is watchdog
-const int pollingGranularity = 4; //how often to poll backend in seconds, 4 makes sense
-const int rebootPollingTimeout = 80; //if no polling in 300 seconds, reboot the ESP
-const int connectionFailureRetrySeconds = 4;
-const int connectionRetryNumber = 22;
-const int allowanceForBoot = 70;
-const int hiatusLengthOfUiUpdatesAfterUserInteraction = 35;
+const char* data_source_host = "your-server.com";
+const int weather_device  = 13;
+const int control_device  = 11; 
+const int polling_granularity = 2; //how often to poll backend in seconds, 4 makes sense
+const int reboot_polling_timeout = 80; //if no polling in 300 seconds, reboot the ESP
+const int connection_retry_number = 22;
+const int allowance_for_boot = 70;
+const int hiatus_length_of_ui_updates_after_user_interaction = 35;
 
-const int weatherRecordingInterval = 3600; //across a whole hour!
-const int weatherUpdateInterval = 67;
-const int energyUpateInterval = 116;
+const int weather_update_interval = 67;
+const int energy_update_interval = 116;
 
-const double temperatureDeltaForChange = 0.2;
-const double pressureDeltaForChange = 0.5;
-const double humidityDeltaForChange = 0.3;
+const double temperature_delta_for_change = 0.2;
+const double pressure_delta_for_change = 0.5;
+const double humidity_delta_for_change = 0.3;
 
-const int backlightTimeout = 600; //how long the backlight stays lit after we start up or a button is pushed.  the backlight uses 100 times more power than the rest of the LCD module
+const int backlight_timeout = 600; //how long the backlight stays lit after we start up or a button is pushed.  the backlight uses 100 times more power than the rest of the LCD module
  
